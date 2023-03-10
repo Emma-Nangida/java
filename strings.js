2e47769cd8e449765fceb6c18813db4e5f2afdf7
Emma-Nangida <emmanangida@gmail.com>
2023-03-10 20:15:56 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,9 @@
+let y = "beautiful";
+console.log(y[0]+y[1]+y[2]+y[3]);
+
+// Insert the following string at the tenth index of the below variable:
+// "eat"
+// const food = "I did not have appetite today"
+let z="I did not have appetite today"
+let r="eat"
+let content.log
